891e50bd058c93acb0228627896d1b01d70e75b5
Omkar Nagvekar <omkarsn18@gmail.com>
2020-04-04 17:07:07 +0530

    Upgrade novelcovid API version

--- server/index.js
+++ server/index.js
@@ -1,29 +1,30 @@
-const express = require('express')
-const covid = require('novelcovid')
-var cors = require('cors')
+const express = require('express');
+const {
+  NovelCovid
+} = require('novelcovid');
+const cors = require('cors');
 
-let app = express()
-
+let app = express();
+const covid = new NovelCovid();
 
 var corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+};
 
-app.use(cors(corsOptions))
-
-app.use(express.static('dist'))
+app.use(cors(corsOptions));
+app.use(express.static('dist'));
 
 app.get('/get-all', async (req, res) => {
-  let countryWiseData = await covid.getCountry()
+  let countryWiseData = await covid.countries();
 
-  const query = req.query
+  const query = req.query;
 
   if (query.limit) {
-    countryWiseData = countryWiseData.slice(0, +query.limit)
+    countryWiseData = countryWiseData.slice(0, +query.limit);
   }
 
-  res.json(countryWiseData)
-})
+  res.json(countryWiseData);
+});
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(8000, () => console.log('Example app listening on port 8000!'));
